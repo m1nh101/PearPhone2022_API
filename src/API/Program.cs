a2a8550a264094cfae2944d94f84934f061f03f4
m1nh <minhdd148@gmail.com>
2022-11-20 06:58:44 +0700

    config swagger

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -32,6 +32,18 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if(app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+
+  app.UseSwaggerUI(options =>
+  {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+  });
+}
+
 app.UseHttpsRedirection();
 
 app.UseCors();
